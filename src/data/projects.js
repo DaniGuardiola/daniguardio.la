45d63a8a96ff76868af74cce9b310ed020e3b7cb
Dani <contact@daniguardiola.me>
2018-09-27 17:45:20 +0200

    just in case

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,21 +1,77 @@
 export default [{
+  key: 'project/coinwallet',
+  title: 'coinwallet: crafting a lightweight Bitcoin wallet app from scratch',
+  tags: ['js', 'react', 'crypto', 'bitcoin']
+}, {
+  key: 'project/bitcoin-lightweight',
+  title: 'bitcoin-lightweight: coding a lightweight wallet library for bitcoin',
+  tags: ['js', 'typescript', 'nodejs', 'web', 'crypto', 'bitcoin']
+}, {
   key: 'project/electrumjs',
-  title: 'electrumjs: an ElectrumX communication layer for Node.js, the browser and React Native',
-  tags: ['js', 'web', 'react-native', 'crypto', 'bitcoin']
+  title: 'electrumjs: creating an ElectrumX client for Node.js and browser',
+  tags: ['js', 'typescript', 'nodejs', 'web', 'crypto', 'bitcoin']
 }, {
   key: 'project/daniguardiola-me',
-  title: 'My personal website (yes, this one)',
+  title: 'daniguardiola.me: building my personal website (yes, this one)',
   tags: ['js', 'web', 'react']
 }, {
-  key: 'project/test3',
-  title: 'portal gun: an Arduino™ powered interdimensional transport device',
-  tags: ['once', 'told', 'me']
+  key: 'project/node-vault',
+  title: 'node-vault: refactoring a Hashicorp Vault javascript client',
+  tags: ['js', 'nodejs', 'security']
 }, {
-  key: 'project/test4',
-  title: 'not-a-flamethrower: definitely not a flamethrower',
-  tags: ['fire', 'flames']
+  key: 'project/timedoser-v1',
+  title: 'TimeDoser v1: building my first app',
+  tags: ['js', 'chrome', 'timedoser', 'design']
 }, {
-  key: 'project/test5',
-  title: 'electrumjs: electrum websocket and tcp socket client',
-  tags: ['js', 'typescript', 'crypto']
+  key: 'project/timedoser-v2',
+  title: 'TimeDoser v2: redesigning and coding a new version from scratch',
+  tags: ['js', 'chrome', 'timedoser', 'design']
+}, {
+  key: 'project/telegram-welcome-bot',
+  title: 'telegram-welcome-bot: building a greeter bot for Telegram groups',
+  tags: ['js', 'nodejs', 'bots']
+}, {
+  key: 'project/incubator',
+  title: 'incubator: crowdsourcing content through a custom platform',
+  tags: ['js', 'php', 'web', 'fun']
+}, {
+  key: 'project/agario-ninja',
+  title: 'Agar.io ninja: hacking a web game to allow keyboard controls',
+  tags: ['js', 'chrome', 'fun']
+}, {
+  key: 'project/weplaybot',
+  title: 'WePlayBot: creating a Twitter bot to play Pokemon Blue (Gameboy)',
+  tags: ['js', 'python', 'chrome', 'fun', 'bots']
+}, {
+  key: 'project/zenflow',
+  title: 'ZenFlow: adding self-destruction functionality to ZenPen',
+  tags: ['js', 'web', 'fun', 'design']
+}, {
+  key: 'project/rektjs',
+  title: 'rektjs: wasting my time in a useless and outdated meme',
+  tags: ['js', 'web', 'fun']
+}, {
+  key: 'project/emoji-wars',
+  title: 'emoji-wars: depriving myself of sleep to build a chatbot game at a hackathon (gone wrong)',
+  tags: ['js', 'web', 'fun']
+}, {
+  key: 'project/altruist-hackathon',
+  title: 'altrui.st (hackathon idea): winning a hackathon with a buzzwordy project',
+  tags: ['js', 'web', 'crypto', 'fun']
+}, {
+  key: 'project/capturechat',
+  title: 'capturechat: capturing Whatsapp conversations with an interactive visor',
+  tags: ['js', 'web', 'fun']
+}, {
+  key: 'project/pencil-material-template',
+  title: 'pencil-material-template: designing a better template for the Pencil prototyping app',
+  tags: ['js', 'web', 'fun', 'design']
+}, {
+  key: 'project/facebook-picture-framer',
+  title: 'Facebook picture framer: framing your Facebook profile picture',
+  tags: ['js', 'web', 'fun']
+}, {
+  key: 'project/playvibe-interface',
+  title: 'PlayVibe interface: adding a settings menu to an open-source Minecraft-enabled sex-toy',
+  tags: ['js', 'web', 'fun']
 }]
