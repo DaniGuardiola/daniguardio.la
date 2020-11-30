0b5c65703dc92a66cd02ef93cc78e2426b6d068f
Dani <hi@daniguardiola.me>
2020-11-30 15:57:25 +0100

    wip

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -17,8 +17,15 @@ export default function Layout ({ children }: PropsWithChildren<{}>) {
         ></meta>
         <link key='favicon' rel='icon' href='favicon.ico' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link href='https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;351;400;700&display=swap' />
-        <link href='https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;351;400;700&display=swap'
+        />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap'
+        />
       </Head>
       <div className='select-none font-slab'>
         <Header
